9f56dff5ed816a4527a367591c28e25f1c69a2c2
slavamlinsky <slavamlinsky@gmail.com>
2024-03-07 18:10:52 +0200

    * updated blog page

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,9 +1,13 @@
+import { getMeal } from "@/lib/meals";
 import Link from "next/link";
 
-function BlogPage() {
+async function BlogPage() {
+  const meal = await getMeal("chicken-nuggets");
+
   return (
     <main>
       <h1>The Blog</h1>
+      <h2>{meal.title}</h2>
       <p>
         <Link href="/blog/post-1">Post 1</Link>
       </p>
