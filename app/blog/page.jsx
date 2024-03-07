f0256dd00d96eca051c798b51d0415228c75d294
slavamlinsky <slavamlinsky@gmail.com>
2024-03-07 16:19:34 +0200

    + add blog links

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,15 +1,23 @@
-
-import Link from 'next/link'
+import Link from "next/link";
 
 function BlogPage() {
   return (
     <main>
-        <h1>The Blog</h1>
-        <p><Link href='/blog/post-1'>Post 1</Link></p>
-        <p><Link href='/blog/post-2'>Post 2</Link></p>
-
+      <h1>The Blog</h1>
+      <p>
+        <Link href="/blog/post-1">Post 1</Link>
+      </p>
+      <p>
+        <Link href="/blog/post-2">Post 2</Link>
+      </p>
+      <p>
+        <Link href="/blog/post-3">Post 3</Link>
+      </p>
+      <p>
+        <Link href="/blog/post-4">Post 4</Link>
+      </p>
     </main>
-  )
+  );
 }
 
-export default BlogPage
+export default BlogPage;
