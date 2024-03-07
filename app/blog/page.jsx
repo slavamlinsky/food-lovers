a0b86879c74e55198762068627e7fcb2ccb59c08
slavamlinsky <slavamlinsky@gmail.com>
2024-03-07 19:21:41 +0200

    * test blog page

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,5 +1,7 @@
 import { getMeal } from "@/lib/meals";
 import Link from "next/link";
+import Image from "next/image";
+import styles from "./page.module.css";
 
 async function BlogPage() {
   const meal = await getMeal("chicken-nuggets");
@@ -7,7 +9,21 @@ async function BlogPage() {
   return (
     <main>
       <h1>The Blog New</h1>
-      <h2>{meal.creator}</h2>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={meal.image} alt={meal.title} fill sizes="20em" />
+        </div>
+        <div className={styles.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={styles.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={styles.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p className={styles.instructions} dangerouslySetInnerHTML={{ __html: meal.instructions }}></p>
+      </main>
       <p>
         <Link href="/blog/post-1">Post 1</Link>
       </p>
