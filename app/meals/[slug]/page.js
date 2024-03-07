51378f96cb1bb739edb97393aac0989c2fc2a777
slavamlinsky <slavamlinsky@gmail.com>
2024-03-07 16:37:05 +0200

    new page

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,35 +1,46 @@
-import { getMeal } from "@/lib/meals";
-import styles from "./page.module.css";
-import Image from "next/image";
-import { notFound } from "next/navigation";
+// import { getMeal } from "@/lib/meals";
+// import styles from "./page.module.css";
+// import Image from "next/image";
+// import { notFound } from "next/navigation";
 
-function MealDetailsPage({ params }) {
-  // const meal = getMeal(params.slug);
+// function MealDetailsPage({ params }) {
+//   const meal = getMeal(params.slug);
 
-  // if (!meal) {
-  //   notFound();
-  // }
-  // meal.instructions = meal.instructions.replace(/\n/g, "<br/>");
+//   if (!meal) {
+//     notFound();
+//   }
+//   meal.instructions = meal.instructions.replace(/\n/g, "<br/>");
+//   return (
+//     <>
+//       {/* <h3>{params.slug}</h3> */}
+//       <header className={styles.header}>
+//         <div className={styles.image}>
+//           <Image src={meal.image} alt={meal.title} fill sizes="20em" />
+//         </div>
+//         <div className={styles.headerText}>
+//           <h1>{meal.title}</h1>
+//           <p className={styles.creator}>
+//             by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+//           </p>
+//           <p className={styles.summary}>{meal.summary}</p>
+//         </div>
+//       </header>
+//       <main>
+//         <p className={styles.instructions} dangerouslySetInnerHTML={{ __html: meal.instructions }}></p>
+//       </main>
+//     </>
+//   );
+// }
+
+// export default MealDetailsPage;
+
+function BlogPostPage({ params }) {
   return (
-    <>
+    <main>
+      <h1>Blog Post Page</h1>
       <h3>{params.slug}</h3>
-      {/* <header className={styles.header}>
-        <div className={styles.image}>
-          <Image src={meal.image} alt={meal.title} fill sizes="20em" />
-        </div>
-        <div className={styles.headerText}>
-          <h1>{meal.title}</h1>
-          <p className={styles.creator}>
-            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
-          </p>
-          <p className={styles.summary}>{meal.summary}</p>
-        </div>
-      </header>
-      <main>
-        <p className={styles.instructions} dangerouslySetInnerHTML={{ __html: meal.instructions }}></p>
-      </main> */}
-    </>
+    </main>
   );
 }
 
-export default MealDetailsPage;
+export default BlogPostPage;
