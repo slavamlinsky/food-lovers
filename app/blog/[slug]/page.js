3c6baab8a6a771378f8f1ebdf71b1191e2dfe1e0
slavamlinsky <slavamlinsky@gmail.com>
2024-03-07 17:50:05 +0200

    * updated pages

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -1,4 +1,10 @@
+import { getMeal } from "@/lib/meals";
+
 function BlogPostPage({ params }) {
+  const meal = getMeal(params.mealSlug);
+
+  console.log(meal);
+
   return (
     <main>
       <h1>Blog Post Page</h1>
---
+++ app/meals/[mealSlug]/page.js
@@ -0,0 +1,35 @@
+import styles from "./page.module.css";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+import { getMeal } from "@/lib/meals";
+
+function MealDetailsPage({ params }) {
+  const meal = getMeal(params.mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
+  meal.instructions = meal.instructions.replace(/\n/g, "<br/>");
+  return (
+    <>
+      <h3>{params.mealSlug}</h3>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={meal.image} alt={meal.title} fill sizes="20em" />
+        </div>
+        <div className={styles.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={styles.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={styles.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p className={styles.instructions} dangerouslySetInnerHTML={{ __html: meal.instructions }}></p>
+      </main>
+    </>
+  );
+}
+
+export default MealDetailsPage;
